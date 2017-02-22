efbe7c08faa254fd0af8cf91edf8b501c8906f07
root <root@envyhax0r.lan>
2017-02-22 13:44:07 +1100

    added a script.

---
+++ apt-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-get -y install pv dialog mc mc-data automake autoconf autopoint autogen autoproject autotools-dev build-essential samba openssh-server \
+irssi irssi-scripts libtool intltool doxygen *-6-multilib bison flex nasm yasm gawk git cvs subversion mercurial zenity vlc ffmpeg perl-base curl \
+python python3 python-pip python3-pip ruby libjson-glib-dev libpurple-dev pidgin finch @#
+exit 0
---
+++ scripts/sendmail.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sendmail ...
+
+
+exit 0
