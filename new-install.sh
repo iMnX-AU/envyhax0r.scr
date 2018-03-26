9dbe3dc23b6db9c4bf7d4335f67eb1df37e0c23e
iMaNeX <imnx.au@outlook.com>
2018-03-27 04:08:51 +1100

    updated new-install.sh...

--- new-install.sh
+++ new-install.sh
@@ -6,18 +6,31 @@ msd0tnet@#.
 EEOF
 
 echo "imnx ALL=NOPASSWD:ALL" > /etc/sudoers.d/myOverrides
+echo "arduino-mk ALL=NOPASSWD:ALL" >> /etc/sudoers.d/myOverrides
+echo "builder ALL=NOPASSWD:ALL" >> /etc/sudoers.d/myOverrides
+
+adduser builder
+adduser builder tty
+adduser builder dialout
+
+adduser arduino-mk
+adduser arduino-mk tty
+adduser arduino-mk dialout
 
 # imanex's package additions (default).
 export pkgs="apt-utils net-tools dnsutils psmisc pv gdisk gparted testdisk iputils-* dialog libncurses5-dev libncursesw5-dev"
-export pkgs="${pkgs} finch mc mc-data irssi irssi-scripts gpm cifs-utils samba openssh-server aptitude exfat-* ntfs-3g hfsplus"
-export pkgs="${pkgs} hfsutils"
+export pkgs="${pkgs} finch mc mc-data irssi irssi-scripts gpm cifs-utils samba openssh-server aptitude exfat-* ntfs-3g"
+export pkgs="${pkgs} hfsplus hfsutils debootstrap vmdebootstrap multistrap qemu-user-static"
+
+export x11_pkgs="pidgin tilda zenity qt5-default qt5-qmake"
 
 # build-essential packages.
-export dev_pkgs="build-essential automake autotools-dev automake autoconf autopoint autogen autoproject libtool intltool git cvs"
-export dev_pkgs="${dev_pkgs} mercurial subversion pandoc libjson-glib-dev libpurple-dev cmake qt5-default qt5-qmake gccgo llvm"
-export dev_pkgs="${dev_pkgs} gfortran gcc-arm-linux-gnueabihf gcc-aarch64-linux-gnu openjdk-8-jdk-headless"
+export dev_pkgs="build-essential automake autotools-dev automake autoconf autopoint autogen autoproject libtool intltool"
+export dev_pkgs="${dev_pkgs} git cvs mercurial subversion pandoc libjson-glib-dev libpurple-dev cmake gccgo llvm"
+export dev_pkgs="${dev_pkgs} gfortran gcc-arm-linux-gnueabihf gcc-aarch64-linux-gnu openjdk-8-jdk-headless bison byacc nasm"
+export dev_pkgs="${dev_pkgs} yasm gawk"
 
-dev_build-dep="ffmpeg vlc"
+export dev_build-dep="ffmpeg vlc kodi ircii youtube-dl"
 
 md5sum /etc/default/grub > .tmpgrub.md5sum
 nano /etc/default/grub
