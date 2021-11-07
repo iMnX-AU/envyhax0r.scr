c7abe6ee587418abf70041e0a199ab100db554fc
iMnX <imnx.au@outlook.com>
2021-11-08 04:00:24 +1100

    Update app-install-script.sh

--- app-install-script.sh
+++ app-install-script.sh
@@ -3,5 +3,5 @@
 apt -y install build-essential automake autoconf autotools-dev autogen m4 bison flex nasm yasm byacc libtool intltool doxygen gfortran gccgo llvm \
 git cvs mercurial subversion pv dialog gdisk testdisk mc mc-data irssi irssi-scripts iptraf-ng net-tools dnsutils iputils-* pandoc zip unzip p7zip-full \
 dialog tilda libncurses5-dev libncursesw5-dev ffmpeg debootstrap vmdebootstrap zenity openjdk-11-jdk-headless ca-certificates-java openssl libssl-dev \
-ca-certificates openssh-server samba samba-vfs-modules python3 python3-pip python3-setuptools python3-dev deluge* libjson-glib-deb libpurple-dev apt-cacher-ng
+ca-certificates openssh-server samba samba-vfs-modules python3 python3-pip python3-setuptools python3-dev deluge* libjson-glib-deb libpurple-dev apt-cacher-ng \
 openssh-server samba samba-vfs-modules
